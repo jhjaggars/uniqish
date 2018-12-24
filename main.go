a1b1d7a9c6b4eb59cb222b6fe2b2ad9f6e0f82d3
Jesse Jaggars <jhjaggars@gmail.com>
2018-12-23 18:05:09 -0600

    using a small lru cache

--- main.go
+++ main.go
@@ -5,31 +5,40 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/hashicorp/golang-lru"
 	"github.com/texttheater/golang-levenshtein/levenshtein"
 )
 
 func main() {
 	counts := make(map[string]int)
-	lines := make(map[string]string)
 	input := bufio.NewScanner(os.Stdin)
+	arc, err := lru.NewARC(16)
+	processed := 0
+	if err != nil {
+		panic(err)
+	}
 	for input.Scan() {
 		line := input.Text()
 		found := false
-		for k := range lines {
-			dist := levenshtein.RatioForStrings([]rune(line), []rune(k), levenshtein.DefaultOptions)
-			if dist > 0.97 {
-				lines[k] = line
-				counts[k]++
+		for _, k := range arc.Keys() {
+			dist := levenshtein.RatioForStrings([]rune(line), []rune(k.(string)), levenshtein.DefaultOptions)
+			if dist >= 0.70 {
+				counts[k.(string)]++
 				found = true
 				break
 			}
 		}
 		if !found {
-			lines[line] = line
 			counts[line] = 1
+			arc.Add(line, nil)
 		}
+		processed++
+		fmt.Fprintf(os.Stderr, "\rProcessed %d lines...", processed)
 	}
+
+	fmt.Fprintf(os.Stderr, "\n")
+
 	for line, n := range counts {
-		fmt.Printf("%d: %s\n", n, line)
+		fmt.Printf("%d\t%s\n", n, line)
 	}
 }
